c26d77818e862a1a73727682f3cd61290f4e862b
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-08-23 17:12:38 +0300

    2

---
+++ LR.WebUI/Controllers/PicturesController.cs
@@ -0,0 +1,16 @@
+﻿using LR.WebUI.Infrastructure;
+using SX.WebCore.MvcControllers;
+using SX.WebCore.ViewModels;
+using System.Web.Mvc;
+
+namespace LR.WebUI.Controllers
+{
+    public class PicturesController : SxPicturesController<DbContext>
+    {
+        public ActionResult Last(int amount = 9)
+        {
+            var data = new SxVMPicture[0];
+            return PartialView("_Last", data);
+        }
+    }
+}
