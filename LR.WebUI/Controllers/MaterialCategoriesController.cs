418fc414a2c4bb8c5e4246528c6504f4e49afd34
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-08-31 08:04:55 +0300

    6

--- LR.WebUI/Controllers/MaterialCategoriesController.cs
+++ LR.WebUI/Controllers/MaterialCategoriesController.cs
@@ -1,9 +1,11 @@
 ﻿using LR.WebUI.Infrastructure;
 using SX.WebCore.MvcControllers;
 using SX.WebCore.ViewModels;
+using System.Web.Mvc;
 
 namespace LR.WebUI.Controllers
 {
+    [AllowAnonymous]
     public sealed class MaterialCategoriesController : SxMaterialCategoriesController<DbContext, SxVMMaterialCategory>
     {
         
