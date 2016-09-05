386a6f3011aeed2bc866a38eadb1317114313507
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-09-05 16:39:52 +0300

    7

---
+++ LR.WebUI/Controllers/SiteNetsController.cs
@@ -0,0 +1,23 @@
+﻿using LR.WebUI.Infrastructure;
+using SX.WebCore.MvcControllers;
+using System.Web.Mvc;
+
+namespace LR.WebUI.Controllers
+{
+    public sealed class SiteNetsController : SxSiteNetsController<DbContext>
+    {
+        [ChildActionOnly]
+        public ActionResult JoinUs()
+        {
+            var viewModel = MvcApplication.SiteNetsProvider.SiteNets;
+            return PartialView("_JoinUs", viewModel);
+        }
+
+        [ChildActionOnly]
+        public ActionResult SocialGroups()
+        {
+            var viewModel = MvcApplication.SiteNetsProvider.SiteNets;
+            return PartialView("_SocialGroups", viewModel);
+        }
+    }
+}
