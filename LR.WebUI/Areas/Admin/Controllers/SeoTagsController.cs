b668bcff3a4a00ff0af2188d8ee1334b663df7e6
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-09-15 09:31:43 +0300

    7

---
+++ LR.WebUI/Areas/Admin/Controllers/SeoTagsController.cs
@@ -0,0 +1,10 @@
+﻿using LR.WebUI.Infrastructure;
+using SX.WebCore.MvcControllers;
+
+namespace LR.WebUI.Areas.Admin.Controllers
+{
+    public sealed class SeoTagsController : SxSeoTagsController<DbContext>
+    {
+        
+    }
+}
---
+++ LR.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+﻿using LR.WebUI.Infrastructure;
+using SX.WebCore.MvcControllers;
+
+namespace LR.WebUI.Controllers
+{
+    public sealed class AccountController : SxAccountController<DbContext>
+    {
+
+    }
+}
