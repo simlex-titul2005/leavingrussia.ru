3fa5e4a2606c377c5551c6338e58540b8e76683e
simlex-titul2005 <simlex.dev.2014@gmail.com>
2016-10-07 15:19:07 +0300

    0

--- LR.WebUI/Controllers/SeoController.cs
+++ LR.WebUI/Controllers/SeoController.cs
@@ -16,10 +16,10 @@ namespace LR.WebUI.Controllers
 
         private string getSeoItemUrl(dynamic model)
         {
-            var mct = (ModelCoreType)model.ModelCoreType;
+            var mct = (byte)model.ModelCoreType;
             switch (mct)
             {
-                case ModelCoreType.Article:
+                case (byte)ModelCoreType.Article:
                     return new VMArticle { DateCreate = model.DateCreate, TitleUrl = model.TitleUrl, ModelCoreType = mct }.GetUrl(Url); ;
                 default:
                     return null;
